1404efc70bc53d1f9c17914360376bca60f4fe58
JovanDyaz <jvaonam@me.com>
2019-09-16 15:17:36 -0500

    firstC

---
+++ dist/main.js
@@ -0,0 +1,8 @@
+
+const input = document.getElementById("input")
+input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault()
+        document.getElementById("button").click()
+    }
+})
